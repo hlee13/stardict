2cc139b58aaf0c28c4a5c9f89ea0050b3a7bf9d0
hlee13 <55885293@qq.com>
2015-04-28 22:50:08 +0800

    add install-stardict-data

---
+++ install-stardict-data.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir -p ~/.stardict/dic/
+
+if [ -f stardict-langdao-ce-gb-2.4.2.tar.bz2 ];then
+    tar jxf stardict-langdao-ce-gb-2.4.2.tar.bz2 -C ~/.stardict/dic/
+fi
+
+if [ -f stardict-langdao-ec-gb-2.4.2.tar.bz2 ];then
+    tar jxf stardict-langdao-ec-gb-2.4.2.tar.bz2 -C ~/.stardict/dic/
+fi
